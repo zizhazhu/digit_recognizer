20665d6e6eacf084d3c8315cb77c22f8d4c7b495
zizhazhu <zizhazhu@gmail.com>
2016-09-09 16:49:17 +0800

    Add some comments.

--- network/__init__.py
+++ network/__init__.py
@@ -2,7 +2,10 @@ import numpy as np
 
 class Network(object):
     def __init__(self, sizes):
+        # sizes is a list of size, the length of sizes is the num of layers
         self.num_layers = len(sizes)
         self.sizes = sizes
+        # np.random.randn generate floats follow gaussian distribution
+        # only nodes in the 1,2,... layers have bias and w
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
